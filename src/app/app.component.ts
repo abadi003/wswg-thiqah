202b13ccb5b484a806d6e634dc0ee50155a4bbfe
Abdullah Aldawsari <a.adawsari@dev.thiqah.sa>
2022-12-01 14:50:58 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+
+import { Component} from '@angular/core';
+import { SelectOption } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'wswg-thiqah';
+export class AppComponent  {
+
+  htmlContent:any
+  sizes: SelectOption[] = [
+    {
+      label: 'A4',
+      value: 'A4',
+    },
+    {
+      label: 'A3',
+      value: 'A3',
+    },
+    {
+      label: 'A3',
+      value: 'A3',
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SelectComponent } from './select.component';
+import { EditorService } from './editor.service';
+import { ToolBarComponent } from './toolbar.component';
+import { EditorComponent } from './ediror.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent,
+    ToolBarComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularEditorModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [EditorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
